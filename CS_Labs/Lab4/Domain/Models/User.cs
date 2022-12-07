02ff736849a6b6a0e0f5262ebcaffbe1f8eed311
Anna C <78998404+AnnaWeber07@users.noreply.github.com>
2022-12-07 11:54:22 +0200

    update

--- CS_Labs/Lab4/Domain/Models/User.cs
+++ CS_Labs/Lab4/Domain/Models/User.cs
@@ -5,8 +5,11 @@
         public string Username { get; set; } = string.Empty;
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
+        public byte[] MessageHash { get; set; }
+        public byte[] MessageSalt { get; set; }
         public string RefreshToken { get; set; } = string.Empty;
         public DateTime TokenCreated { get; set; }
         public DateTime TokenExpires { get; set; }
+        public string Message { get; set; }
     }
 }
