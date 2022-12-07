6d51904d9d1c1333db45b15b02b624451d214354
Anna C <78998404+AnnaWeber07@users.noreply.github.com>
2022-12-07 11:53:14 +0200

    update

---
+++ CS_Labs/Lab4/Domain/DTO/GetMessageDTO.cs
@@ -0,0 +1,12 @@
+﻿using AnnaWebweJWTandHashingPassword.Domain.Models;
+
+namespace AnnaWebweJWTandHashingPassword.Domain.DTO
+{
+    public class GetMessageDTO
+    {
+        public string token { get; set; } = string.Empty;
+        public string Username { get; set; }
+        public string Message { get; set; }
+        
+    }
+}
---
+++ CS_Labs/Lab4/Domain/DTO/WriteMessageDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace AnnaWebweJWTandHashingPassword.Domain.DTO
+{
+    public class WriteMessageDTO
+    {
+        public string token { get; set; } = string.Empty;
+        public string Username { get; set; }
+        public string Message { get; set; } = String.Empty;
+    }
+}
